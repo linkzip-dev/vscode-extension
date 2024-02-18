48a6e17724a3fa78cb94a46d6e6fec1d7518b9f8
zombocoder <root@zombocoder.com>
2024-02-18 18:58:55 +0200

    Add new features

--- src/extension.ts
+++ src/extension.ts
@@ -1,59 +1,55 @@
-/*---------------------------------------------------------
- * Copyright (C) Microsoft Corporation. All rights reserved.
- *--------------------------------------------------------*/
+import * as vscode from "vscode";
+import { handleInitStatusBarItemClick } from "./handlers/initProject";
+import { handleDeployStatusBarItemClick } from "./handlers/deployProject";
+import { RenderStatusBarItem } from "./components/statusbar/render";
+import { VsCodeCommands } from "./types";
+import { handleConfigureStatusBarItemClick } from "./handlers/configureLinkZip";
+import { configureCommandId, deployCommandId, initCommandId } from "./consts";
 
-import * as vscode from 'vscode';
+export function activate(context: vscode.ExtensionContext) {
+  // Create status bar item
+  const extStatusBarItem = vscode.window.createStatusBarItem(
+    vscode.StatusBarAlignment.Right,
+    100
+  );
+  extStatusBarItem.text = `LinkZip: Init Project`;
+  extStatusBarItem.command = initCommandId;
+  extStatusBarItem.show();
+  context.subscriptions.push(extStatusBarItem);
 
-let myStatusBarItem: vscode.StatusBarItem;
+  // Register listeners
+  context.subscriptions.push(
+    vscode.window.onDidChangeActiveTextEditor(updateStatusBarItem)
+  );
+  context.subscriptions.push(
+    vscode.window.onDidChangeTextEditorSelection(updateStatusBarItem)
+  );
+  context.subscriptions.push(
+    vscode.window.onDidChangeWindowState(updateStatusBarItem)
+  );
 
-export function activate({ subscriptions }: vscode.ExtensionContext) {
-	// register a command that is invoked when the status bar
-	// item is selected
-	const myCommandId = 'sample.showSelectionCount';
-	subscriptions.push(
-		vscode.commands.registerCommand(myCommandId, () => {
-			const n = getNumberOfSelectedLines(vscode.window.activeTextEditor);
-			vscode.window.showInformationMessage(
-				`Yeah, ${n} line(s) selected... Keep going!`
-			);
-		})
-	);
+  // Update status bar item once at start
+  updateStatusBarItem();
 
-	// create a new status bar item that we can now manage
-	myStatusBarItem = vscode.window.createStatusBarItem(
-		vscode.StatusBarAlignment.Right,
-		100
-	);
-	myStatusBarItem.command = myCommandId;
-	subscriptions.push(myStatusBarItem);
+  // Register the new command for handling input dialog
+  context.subscriptions.push(
+    vscode.commands.registerCommand(
+      configureCommandId,
+      handleConfigureStatusBarItemClick
+    )
+  );
+  context.subscriptions.push(
+    vscode.commands.registerCommand(initCommandId, handleInitStatusBarItemClick)
+  );
+  context.subscriptions.push(
+    vscode.commands.registerCommand(
+      deployCommandId,
+      handleDeployStatusBarItemClick
+    )
+  );
 
-	// register some listener that make sure the status bar
-	// item always up-to-date
-	subscriptions.push(vscode.window.onDidChangeActiveTextEditor(updateStatusBarItem));
-	subscriptions.push(vscode.window.onDidChangeTextEditorSelection(updateStatusBarItem));
-
-	// update status bar item once at start
-	updateStatusBarItem();
-}
-
-function updateStatusBarItem(): void {
-	const files = vscode.workspace.findFiles('**/linkzip.json').then((file) => {
-		if (file.length > 0) {
-			myStatusBarItem.text = `LinkZip: Deploy build`;
-			myStatusBarItem.show();
-		} else {
-			myStatusBarItem.hide();
-		}
-	});
-}
-
-function getNumberOfSelectedLines(editor: vscode.TextEditor | undefined): number {
-	let lines = 0;
-	if (editor) {
-		lines = editor.selections.reduce(
-			(prev, curr) => prev + (curr.end.line - curr.start.line),
-			0
-		);
-	}
-	return lines;
+  // Update status bar item based on active editor
+  function updateStatusBarItem() {
+    RenderStatusBarItem(extStatusBarItem);
+  }
 }
